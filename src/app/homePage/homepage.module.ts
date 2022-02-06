e70ed9dc0b43d376dc8e9912b80d1527bedbeffd
helderlim <helderfelima16@gmail.com>
2022-02-05 21:16:46 -0300

    first commit

---
+++ src/app/homePage/homepage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { HomepageComponent } from "./homepage.component";
+
+  @NgModule({
+    declarations: [
+    ],
+    imports: [
+    ],
+    exports: [
+      HomepageComponent
+    ]
+})
+export class HomepageModule {}
